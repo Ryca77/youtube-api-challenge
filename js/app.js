48abbd9dbb7365541e207be32676f226872e7343
Ryca77 <ryan.carter77@gmail.com>
2016-06-20 18:03:14 +0100

    js/app.js

--- js/app.js
+++ js/app.js
@@ -28,9 +28,11 @@ function getRequest (searchTerm) {
 		var image = listings.snippet.thumbnails.medium.url
 		var id = listings.id.videoId
 		var title = listings.snippet.title
+		var channel = listings.snippet.channelTitle
 		$('#listings').append('<li><a href="https://www.youtube.com/watch?v=' + id + '" target="_blank"><h3>' + title + '</h3><img src="' + image + '" width="120" height="90"></a></li>');
+		$('#listings').append('<li><a href="https://www.youtube.com/user/' + channel + '" target="_blank">' + channel + '</a></li>');
 		$('#listings').css('list-style-type', 'none');
-		console.log(listings.snippet.thumbnails.medium.url);
+		console.log(channel);
 		});
 	});
 }
