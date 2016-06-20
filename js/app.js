87830cc235fd696be54435ab448a8b9cdad4cc73
Ryca77 <ryan.carter77@gmail.com>
2016-06-20 17:02:49 +0100

    js/app.js

---
+++ js/app.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+
+
+
+$(function() {
+	$('#search-term').submit(function(event) { 
+		event.preventDefault();
+		var searchTerm = $('#query').val();
+		getRequest(searchTerm);
+		});
+	});
+
+function getRequest (searchTerm) {
+	var params = {
+		part: 'snippet',
+		key: 'AIzaSyA9EP9RSTFp1Waw9-9wGHtFlB2v2t8qJPY',
+		maxResults: 20,
+		q: searchTerm
+	};
+	url = 'https://www.googleapis.com/youtube/v3/search';
+
+	$.getJSON(url, params, function(data) {
+	
+	console.log(data);
+	console.log(data.items[0].snippet.title);
+	
+	$.each(data.items, function(index, listings) {
+		var image = listings.snippet.thumbnails.medium.url
+		var id = listings.id.videoId
+		var title = listings.snippet.title
+		$('#listings').append('<li><a href="https://www.youtube.com/watch?v=' + id + '" target="_blank"><h3>' + title + '</h3><img src="' + image + '" width="120" height="90"></a></li>');
+		$('#listings').css('list-style-type', 'none');
+		console.log(listings.snippet.thumbnails.medium.url);
+		});
+	});
+}
+
+});
+
+/*
+var list = ""
+		$.each(data.items, function(index, titles) {
+			list += "<p>" + titles.snippet.title + "</p>";
+			console.log(titles.snippet.title);
+			console.log(titles.id.videoId);
+			});
+		$('#search-results').html(list);*/
